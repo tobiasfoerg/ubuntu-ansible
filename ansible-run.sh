d5b6e3da809d54914598496baf95eb8ad9cf7cfd
Tobias Förg <tobias.foerg@gmail.com>
2022-06-29 10:52:10 +0200

    add gh cli

--- ansible-run.sh
+++ ansible-run.sh
@@ -4,3 +4,6 @@
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update -y
 sudo apt-get install -y curl git software-properties-common ansible
+
+## Run playbook
+ansible-playbook local.yml -K
